afddcc935d6165634e41ea52ebaa657ba2baa48e
Mamta-PC <Mamta-PC@DESKTOP-5FH3MB0>
2017-03-18 13:39:59 +0530

    Initial commit

---
+++ src/e/Demo.java
@@ -0,0 +1,13 @@
+package e;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void test(){
+		Reporter.log("This is a demo",true);
+	}
+
+}
