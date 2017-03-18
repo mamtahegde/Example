194e0420d3c7a35bb537dfb309d16fcde0be5ff1
mamtahegde <dmamta419@gmail.com>
2017-03-18 13:47:50 +0530

    Create Demo1.java

---
+++ src/e/Demo1.java
@@ -0,0 +1,14 @@
+package e;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test
+	public void test(){
+		Reporter.log("This is a demo 1",true);
+//		
+	}
+
+}
